7c72492ee815548da3b6ce0ec58e1f955c6692f1
Centuriononon <centuriononon@gmail.com>
2023-08-21 20:15:38 +0400

    [update] 2023-08-21_20-15-38

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -34,42 +34,131 @@ vim.g.vimtex_compiler_latexmk = {
 vim.g.vimtex_view_method = "zathura"
 vim.g.latex_view_general_viewer = "zathura"
 
--- Helpers
-
-
 -- Log
 Log = {}
 
-function Log:log_table(table) 
+function Log.log_table(table) 
   for key, value in pairs(table) do
-    print(key, ":", value) 
+    print(key, ":", value)
   end
 end
 
--- FileSave 
-FileSave = {}
+-- Events
+VimEventsHelper = {}
 
-function FileSave:register(group, fn)
-  vim.api.nvim_exec(
-    "augroup " .. group .. " " 
-    .. "autocmd! " 
-    .. "autocmd BufWritePost * lua " .. fn .. " "
-    .. "augroup END", 
-    false
+function VimEventsHelper.get_sub_script(group, event, func)
+  return string.format([[
+    augroup %s 
+      autocmd! %s
+      autocmd %s * lua %s 
+    augroup END
+  ]], group, event, event, func)
+end
+
+function VimEventsHelper.get_unsub_script(group, event)
+  return string.format([[
+    augroup %s 
+      autocmd! %s 
+    augroup END
+  ]], group, event)
+end
+
+function VimEventsHelper.sub(group, event, func)
+  local script = VimEventsHelper.get_sub_script(group, event, func)
+  vim.api.nvim_exec(script, false)
+end
+
+function VimEventsHelper.unsub(group, event)
+  local script = VimEventsHelper.get_unsub_script(group, event) 
+  vim.api.nvim_exec(script, false)
+end
+
+function VimEventsHelper.format_group_name(str)
+  return string.gsub(str, "[%p%c]", "_")  
+end
+
+-- VimEvents
+VimEvents = {}
+_VimEventsRegistry = {}
+
+function VimEvents.pub(group, event)
+  local registry = _VimEventsRegistry
+
+  if registry[group] and registry[group][event] then 
+    local cb_list = registry[group][event]
+   
+    Log.log_table(cb_list)
+    for i = 1, #cb_list do
+      cb_list[i]() 
+    end
+  end 
+end
+
+function VimEvents.sub(group, event, cb)
+  local registry = _VimEventsRegistry
+
+  if not registry[group] or not registry[group][event] then
+    registry[group] = {}
+    registry[group][event] = {}
+  end
+
+  table.insert(registry[group][event], cb)
+
+  local func = string.format(
+    [[VimEvents.pub("%s", "%s")]], 
+    group, event
   )
+
+  VimEventsHelper.sub(group, event, func) 
 end
 
-function FileSave:deregister(group) 
-  vim.api.nvim_exec(
-    [[augroup ]] .. group .. [[ autocmd! augroup END]], 
-    false
-  ) 
+function VimEvents.unsub(group, event)
+  local registry = _VimEventsRegistry
+  
+  VimEventsHelper.unsub(group, event)
+  
+  if registry[group] then
+    registry[group][event] = nil 
+  end
 end
 
--- Custom Texer
-Tex = {}
+-- VimEvent
+VimEvent = {}
 
-function Tex:_run_commands_in_bg(commands, run_handler, end_handler)
+function VimEvent:new(event_name)
+  local state = {
+    event = event_name 
+  }
+  setmetatable(state, self)
+  self.__index = self
+
+  return state
+end
+
+function VimEvent:_pub(group)
+  VimEvents.pub(group, self.event)
+end
+
+function VimEvent:sub(name, cb)
+  local group = VimEventsHelper.format_group_name(name)
+  VimEvents.sub(group, self.event, cb)
+end
+
+function VimEvent:unsub(name) 
+  local group = VimEventsHelper.format_group_name(name)
+  VimEvents.unsub(group, self.event)
+end
+
+-- FileSaveEvent
+FileSaveEvent = VimEvent:new("BufWritePost") 
+
+-- ExitEvent
+ExitEvent = VimEvent:new("VimLeavePre")
+
+-- Commander
+Commander = {}
+
+function Commander.run_in_bg(commands, run_handler, end_handler)
   local cmd_id = 1
   
   local function run_cmd(cb)
@@ -101,7 +190,8 @@ function Tex:_run_commands_in_bg(commands, run_handler, end_handler)
   return run_commands() 
 end
 
-function Tex:_run_commands(...)
+
+function Commander.run(...)
   commands = {...} 
 
   if #commands == 0 then
@@ -117,118 +207,189 @@ function Tex:_run_commands(...)
   return fn.system(row)
 end
 
-function Tex:_get_make_compile_dir_command(output)
+function Commander.stop(id)
+  fn.jobstop(id)
+end
+
+-- TexHelper
+TexHelper = {}
+
+function TexHelper.get_mk_cmp_dir_cmd(output)
   return 'mkdir -p ' .. output
 end
 
-function Tex:_get_compile_command(file, output)
+function TexHelper.get_cmp_cmd(file, output)
   return 'pdflatex -output-directory=' .. output .. ' ' .. file 
 end
 
-function Tex:_get_view_command(pdf)
+function TexHelper.get_view_cmd(pdf)
   return 'zathura' .. ' ' .. pdf
 end
 
-function Tex:_get_clean_command(output)
+function TexHelper.get_clean_cmd(output)
   return 'rm -r ' .. output
 end
 
-function Tex:new()
-  local state = {
-    sessions = {}
-  }
-  setmetatable(state, self)
-  self.__index = self
+function TexHelper.get_ctx()
+  local fname = fn.expand('%:r')
+  local tex_file = fname .. '.tex'
+  local fpath = fn.fnameescape(vim.fn.expand('%:p'))
+  local curr_dir = fn.expand('%:p:h')
+  local cmp_dir = curr_dir .. '/' .. 'compiled_' .. fname
+  local pdf_path = cmp_dir .. '/' .. fname .. '.pdf'
 
+  return {
+    fname = fname,
+    tex_file = tex_file,
+    fpath = fpath,
+    curr_dir = curr_dir,
+    cmp_dir = cmp_dir,
+    pdf_path = pdf_path
+  }
+end
+
+-- Tex
+Tex = {}
+
+function Tex.compile()
+  local ctx = TexHelper.get_ctx()
+
+  local mk_cmp_dir = TexHelper.get_mk_cmp_dir_cmd(ctx.cmp_dir) 
+  local cmp = TexHelper.get_cmp_cmd(ctx.tex_file, ctx.cmp_dir)
+  
+  Commander.run(mk_cmp_dir, cmp)	
+end
+
+function Tex.view()
+  local ctx = TexHelper.get_ctx()
+  
+  local view = TexHelper.get_view_cmd(ctx.pdf_path)
+
+  Commander.run_in_bg({view})
+end
+
+function Tex.clean()
+  local ctx = TexHelper.get_ctx()
+  local clean = TexHelper.get_clean_cmd(ctx.cmp_dir)
+  Commander.run(clean)
+end
+
+-- TexObserver
+TexObserver = {}
+
+function TexObserver:new()
+  local state = { }
+  setmetatable(state, self)
+  self.__index = self 
   return state
 end
 
-function Tex:_get_context()
-    local fname = fn.expand('%:r')
-    local tex_file = fname .. '.tex'
-    local fpath = fn.fnameescape(vim.fn.expand('%:p'))
-    local curr_dir = fn.expand('%:p:h')
-    local cmp_dir = curr_dir .. '/' .. 'compiled_' .. fname
-    local pdf_path = cmp_dir .. '/' .. fname .. '.pdf'
-
-    return {
-      fname = fname,
-      tex_file = tex_file,
-      fpath = fpath,
-      curr_dir = curr_dir,
-      cmp_dir = cmp_dir,
-      pdf_path = pdf_path
-    }
+function TexObserver:is_on(key)
+  local ctx = TexHelper.get_ctx()
+  return self[ctx.fpath] and self[ctx.fpath][key]
 end
 
-function Tex:compile()
-  local ctx = Tex:_get_context()
-
-  local mk_cmp_dir = Tex:_get_make_compile_dir_command(ctx.cmp_dir) 
-  local cmp = Tex:_get_compile_command(ctx.tex_file, ctx.cmp_dir)
-  
-  Tex:_run_commands(mk_cmp_dir, cmp)	
+function TexObserver:enable_compile_on_save()
+  local ctx = TexHelper.get_ctx()
+ 
+  if not self:is_on("compile_on_save") then
+    FileSaveEvent:sub(ctx.fpath, function() Tex.compile() end)
+    self[ctx.fpath] = { compile_on_save = true }
+  end
 end
 
-function Tex:view()
-  local ctx = Tex:_get_context()
-  
-  local view = Tex:_get_view_command(ctx.pdf_path)
+function TexObserver:disable_compile_on_save()
+  local ctx = TexHelper.get_ctx()
 
-  Tex:_run_commands_in_bg({view})
+  if self:is_on("compile_on_save") then
+    FileSaveEvent:unsub(ctx.fpath)
+    self[ctx.fpath].compile_on_save = nil 
+  end
 end
 
-function Tex:clean()
-  local ctx = Tex:_get_context()
-
-  local clean = Tex:_get_clean_command(ctx.cmp_dir)
+function TexObserver:enable_clean_on_exit()
+  local ctx = TexHelper.get_ctx()
   
-  Tex:_run_commands(clean)
+  if not self:is_on("clean_on_exit") then
+    ExitEvent:sub(ctx.fpath, function() Tex.clean() end) 
+    self[ctx.fpath] = { clean_on_exit = true }
+  end
 end
 
-function Tex:flow()
-  local ctx = Tex:_get_context()
-
-  Tex:compile()	
+function TexObserver:disable_clean_on_exit()
+  local ctx = TexHelper.get_ctx()
   
-  local is_new = self.sessions[ctx.fpath] == nil
-  
-  if is_new then
-    local view = Tex:_get_view_command(ctx.pdf_path)
+  if self:is_on("clean_on_exit") then
+    ExitEvent:unsub(ctx.fpath) 
+    self[ctx.fpath].clean_on_exit = nil 
+  end
+end
 
-    local run_handler = function(_cmd_id, job_id)
+
+-- TexSessions
+TexSessions = {}
+
+function TexSessions:new(observer)
+  local state = { 
+    observer = observer,
+    sessions = {} 
+  }
+  setmetatable(state, self)
+  self.__index = self 
+  return state
+end
+
+function TexSessions:compile_view_clean()
+
+  local ctx = TexHelper.get_ctx()
+
+  Tex.compile()	
+
+  if not self.sessions[ctx.fpath] then
+    local view_cmd = TexHelper.get_view_cmd(ctx.pdf_path)
+
+    local run_handler = function(_cmd_id, id)
       self.sessions[ctx.fpath] = {
-	view_job_id = job_id 
+	view_id = id 
       } 
     end
 
     local end_handler = function()
-      Tex:clean()
-      self.sessions[fpath] = nil
+      Tex.clean()
+      self.sessions[ctx.fpath] = nil
     end
     
-    Tex:_run_commands_in_bg({view}, run_handler, end_handler)
+    Commander.run_in_bg({view_cmd}, run_handler, end_handler)
   end
 end
 
-function Tex:stop_flow()
-  local ctx = Tex:_get_context()
+function TexSessions:stop_view()
+  local ctx = TexHelper.get_ctx()
   
   if self.sessions[ctx.fpath] then
-    view_job_id = self.sessions[ctx.fpath].view_job_id 
-
-    fn.jobstop(view_job_id)
-
+    view_id = self.sessions[ctx.fpath].view_id 
+    Commander.stop(view_id)
     self.sessions[ctx.fpath] = nil
   end
-
-  Tex:clean() 
 end
 
-tex = Tex:new()
+function TexSessions:flow()
+  self.observer:enable_clean_on_exit()
+  self.observer:enable_compile_on_save()
+  self:compile_view_clean()
+end
 
-vim.cmd('command! -nargs=0 TexFlow :lua tex:flow()')
-vim.cmd('command! -nargs=0 TexStop :lua tex:stop_flow()')
-vim.cmd('command! -nargs=0 TexCompile :lua tex:compile()')
-vim.cmd('command! -nargs=0 TexView :lua tex:view()')
-vim.cmd('command! -nargs=0 TexClean :lua tex:clean()')
+tex_observer = TexObserver:new()
+tex_sessions = TexSessions:new(tex_observer)
+
+vim.cmd('command! -nargs=0 TexCompile :lua Tex.compile()')
+vim.cmd('command! -nargs=0 TexView :lua Tex.view()')
+vim.cmd('command! -nargs=0 TexClean :lua Tex.clean()')
+
+vim.cmd('command! -nargs=0 TexEnableCompileOnSave :lua tex_observer:enable_compile_on_save()')
+vim.cmd('command! -nargs=0 TexDisableCompileOnSave :lua tex_observer:disable_compile_on_save()')
+vim.cmd('command! -nargs=0 TexEnableCleanOnExit :lua tex_observer:enable_clean_on_exit()')
+vim.cmd('command! -nargs=0 TexDisableCleanOnExit :lua tex_observer:disable_clean_on_exit()')
+
+vim.cmd('command! -nargs=0 Tex :lua tex_sessions:compile_view_clean()')
+vim.cmd('command! -nargs=0 TexFlow :lua tex_sessions:flow()')
